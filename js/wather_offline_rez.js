3f3990df64c2fa56b66f9b3dc9821da2402470cd
thehatter <andrewlutenko@gmail.com>
2016-03-17 21:09:39 +0300

    add ajax to wather.js

---
+++ js/wather_offline_rez.js
@@ -0,0 +1,158 @@
+
+var xml = '<?xml version="1.0" encoding="utf-8"?> <weather exp_time="300"> <location id="4218" name="Витебск" name_r="в Витебске" tzone="180" cur_time="2016-03-17T18:26:52"> <day date="2016-03-17" risem="424" setm="1149" durm="725" tmin="2" tmax="5" pmin="742" pmax="749" wsmin="2" wsmax="6" hummin="72" hummax="94" cl="3" pt="1" pr="2" ts="0" icon="d.sun.c4.r2" descr="Пасмурно, дождь" p="742" ws="3" wd="8" hum="75" grademax="3"> <forecast valid="2016-03-17T21:00:00" tod="3"><values t="4" p="743" ws="5" wd="8" hum="75" hi="1" cl="3" pt="0" pr="0" ts="0" icon="n.moon.c4" descr="Пасмурно" grade="2"/></forecast> </day> <day date="2016-03-18" risem="422" setm="1151" durm="729" tmin="-4" tmax="3" pmin="736" pmax="740" wsmin="4" wsmax="6" hummin="38" hummax="83" cl="3" pt="2" pr="2" ts="0" icon="d.sun.c4.s2" descr="Пасмурно, снег" p="736" ws="5" wd="1" hum="61" grademax="3"> <forecast valid="2016-03-18T03:00:00" tod="0"><values t="2" p="739" ws="6" wd="8" hum="83" hi="-1" cl="3" pt="0" pr="0" ts="0" icon="n.moon.c4" descr="Пасмурно" grade="2"/></forecast> <forecast valid="2016-03-18T09:00:00" tod="1"><values t="1" p="737" ws="5" wd="8" hum="74" hi="-2" cl="3" pt="2" pr="1" ts="0" icon="d.sun.c4.s1" descr="Пасмурно, небольшой снег" grade="2"/></forecast> <forecast valid="2016-03-18T15:00:00" tod="2"><values t="0" p="736" ws="5" wd="1" hum="61" hi="-4" cl="3" pt="2" pr="2" ts="0" icon="d.sun.c4.s2" descr="Пасмурно, снег" grade="2"/></forecast> <forecast valid="2016-03-18T21:00:00" tod="3"><values t="-4" p="738" ws="4" wd="1" hum="38" hi="-7" cl="1" pt="2" pr="1" ts="0" icon="n.moon.c2.s1" descr="Малооблачно, небольшой снег" grade="2"/></forecast> </day> <day date="2016-03-19" risem="419" setm="1153" durm="734" tmin="-5" tmax="-1" pmin="740" pmax="743" wsmin="3" wsmax="6" hummin="37" hummax="51" cl="3" pt="2" pr="1" ts="0" icon="d.sun.c4.s1" descr="Пасмурно, небольшой снег" p="743" ws="6" wd="8" hum="46" grademax="2"> <forecast valid="2016-03-19T03:00:00" tod="0"><values t="-5" p="741" ws="4" wd="1" hum="49" hi="-8" cl="1" pt="0" pr="0" ts="0" icon="n.moon.c2" descr="Малооблачно" grade="2"/></forecast> <forecast valid="2016-03-19T09:00:00" tod="1"><values t="-4" p="742" ws="6" wd="1" hum="47" hi="-9" cl="3" pt="0" pr="0" ts="0" icon="d.sun.c4" descr="Пасмурно" grade="2"/></forecast> <forecast valid="2016-03-19T15:00:00" tod="2"><values t="-1" p="743" ws="6" wd="8" hum="46" hi="-5" cl="1" pt="2" pr="1" ts="0" icon="d.sun.c2.s1" descr="Малооблачно, небольшой снег" grade="1"/></forecast> <forecast valid="2016-03-19T21:00:00" tod="3"><values t="-5" p="743" ws="3" wd="7" hum="43" hi="-7" cl="0" pt="0" pr="0" ts="0" icon="n.moon" descr="Ясно" grade="2"/></forecast> </day> <day date="2016-03-20" risem="416" setm="1155" durm="738" tmin="-7" tmax="-1" pmin="741" pmax="744" wsmin="2" wsmax="6" hummin="35" hummax="59" cl="3" pt="2" pr="1" ts="0" icon="d.sun.c4.s1" descr="Пасмурно, небольшой снег" p="743" ws="5" wd="8" hum="35"> <forecast valid="2016-03-20T03:00:00" tod="0"><values t="-6" p="741" ws="2" wd="7" hum="56" hi="-7" cl="0" pt="0" pr="0" ts="0" icon="n.moon" descr="Ясно"/></forecast> <forecast valid="2016-03-20T09:00:00" tod="1"><values t="-4" p="742" ws="3" wd="8" hum="49" hi="-7" cl="0" pt="0" pr="0" ts="0" icon="d.sun" descr="Ясно"/></forecast> <forecast valid="2016-03-20T15:00:00" tod="2"><values t="-1" p="743" ws="5" wd="8" hum="35" hi="-4" cl="3" pt="0" pr="0" ts="0" icon="d.sun.c4" descr="Пасмурно"/></forecast> <forecast valid="2016-03-20T21:00:00" tod="3"><values t="-4" p="744" ws="2" wd="8" hum="53" hi="-5" cl="2" pt="0" pr="0" ts="0" icon="n.moon.c3" descr="Облачно"/></forecast> </day> <day date="2016-03-21" risem="414" setm="1157" durm="743" tmin="-6" tmax="2" pmin="741" pmax="744" wsmin="1" wsmax="4" hummin="35" hummax="70" cl="3" pt="0" pr="0" ts="0" icon="d.sun.c4" descr="Пасмурно" p="743" ws="4" wd="6" hum="35"> <forecast valid="2016-03-21T03:00:00" tod="0"><values t="-5" p="744" ws="1" wd="7" hum="53" hi="-5" cl="3" pt="0" pr="0" ts="0" icon="n.moon.c4" descr="Пасмурно"/></forecast> <forecast valid="2016-03-21T09:00:00" tod="1"><values t="-4" p="744" ws="2" wd="6" hum="51" hi="-5" cl="0" pt="0" pr="0" ts="0" icon="d.sun" descr="Ясно"/></forecast> <forecast valid="2016-03-21T15:00:00" tod="2"><values t="2" p="743" ws="4" wd="6" hum="35" hi="0" cl="0" pt="0" pr="0" ts="0" icon="d.sun" descr="Ясно"/></forecast> <forecast valid="2016-03-21T21:00:00" tod="3"><values t="-1" p="741" ws="4" wd="6" hum="70" hi="-4" cl="2" pt="0" pr="0" ts="0" icon="n.moon.c3" descr="Облачно"/></forecast> </day> <day date="2016-03-22" risem="411" setm="1159" durm="747" tmin="-3" tmax="2" pmin="738" pmax="743" wsmin="2" wsmax="5" hummin="53" hummax="95" cl="3" pt="2" pr="2" ts="0" icon="d.sun.c4.s2" descr="Пасмурно, снег" p="740" ws="4" wd="1" hum="57"> <forecast valid="2016-03-22T03:00:00" tod="0"><values t="0" p="739" ws="5" wd="6" hum="86" hi="-3" cl="3" pt="2" pr="1" ts="0" icon="n.moon.c4.s1" descr="Пасмурно, небольшой снег"/></forecast> <forecast valid="2016-03-22T09:00:00" tod="1"><values t="0" p="739" ws="4" wd="7" hum="89" hi="-2" cl="3" pt="2" pr="2" ts="0" icon="d.sun.c4.s2" descr="Пасмурно, снег"/></forecast> <forecast valid="2016-03-22T15:00:00" tod="2"><values t="2" p="740" ws="4" wd="1" hum="57" hi="0" cl="2" pt="2" pr="1" ts="0" icon="d.sun.c3.s1" descr="Облачно, небольшой снег"/></forecast> </day> </location></weather>';
+
+
+
+
+
+(function ($) {
+  $(document).ready(function(){
+
+    
+    xml = $.parseXML( xml );
+    
+    var test = $(xml).find('weather').attr('exp_time');
+    console.log(test)
+
+
+    //$.get( "wather.xm", function( data ) {
+
+    //});
+
+    // create array to hold day 
+    var weekday = new Array();
+    weekday[0] = "Воскресенье";
+    weekday[1] = "Вторник";
+    weekday[2] = "Среда";
+    weekday[3] = "Четверг";
+    weekday[4] = "Пятница";
+    weekday[5] = "Суббота";
+    weekday[6] = "Понедельник";
+
+
+    var WGen = {
+      // format temperatured
+      FormatT: function (t) {
+        var intT = parseInt(t);
+        if (intT > 0){
+          return '+' + t;
+        }
+        else { return t}
+      },
+      // create html img icon from code
+      genIcon: function(i) {
+        return '<img src="http://st7.gisstatic.ru/static/images/icons/new/'+ i +'.png">';
+      },
+
+      toDate: function(dateStr) {
+        var parts = dateStr.split("-");
+        return new Date(parts[0], parts[1] - 1, parts[2]);
+      },
+
+
+
+      // createSmallWidget get place (target string with selector) and parsed xml
+      // then crete mini widget with icon and temperature
+      createSmallWidget: function (place, xml) {
+        //var currentTime = new Date();
+
+        var currentTemperature = $(xml).find('day:eq(0) forecast:eq(0) values').attr('t');
+        var currentIcon = $(xml).find('day:eq(0) forecast:eq(0) values').attr('icon');
+
+        var formatedTemperature = WGen.FormatT(currentTemperature);
+
+        var widgetBody = ' \
+          <div class="img">'+ WGen.genIcon(currentIcon) +'</div> \
+          <div class="temperature ">'+ formatedTemperature +'</div> \
+        '
+        
+        $(place).append(widgetBody);
+      },
+
+
+
+      createBigWidget: function (place, xml) {
+        
+        var weekDays = $(xml).find('day');
+        var weekdaysHolder = ''
+        var daytimeNumber = new Array();
+        daytimeNumber[0] = "Ночь";
+        daytimeNumber[1] = "Утро";
+        daytimeNumber[2] = "День";
+        daytimeNumber[3] = "Вечер";
+
+
+        
+
+
+        $(weekDays).each(function(index) {
+          var forecasts = $(this).find('forecast');
+
+          var dayDate = WGen.toDate($(this).attr('date'));
+          var dayDateName = weekday[dayDate.getDay()];
+          var dayDateNumber = dayDate.getDate();
+
+          var dayHolder = ''
+
+
+          $(forecasts).each(function(index) {
+            var forecastTod = $(this).attr('tod')
+            var forecastTimeName = daytimeNumber[forecastTod]
+            var forecastIcon = $(this).find('values').attr('icon');
+            var forecastTemperature = $(this).find('values').attr('t');
+            var forecastDescr = $(this).find('values').attr('descr');
+
+
+            var forecastBody = ' \
+              <div class="weekday_h  clearfix tod_'+ forecastTod +'"> \
+                <div class="title"> '+ forecastTimeName +': </div> \
+                <div class="icon">'+ WGen.genIcon(forecastIcon) +'</div> \
+                <div class="temperature">'+ WGen.FormatT(forecastTemperature) +'</div> \
+                <div class="descr"><div class="cell">'+ forecastDescr +'</div></div> \
+              </div> \
+              ';
+            dayHolder = dayHolder + forecastBody;
+
+          });
+
+          
+          var dayBody = ' \
+            <div class="weekday clearfix"> \
+              <div class="date_cont"> \
+                <div class="date"> '+ dayDateNumber +' </div> \
+                <div class="dayname"> '+ dayDateName +' </div> \
+              </div> \
+              '+ dayHolder +' \
+            </div> \
+          ';
+          weekdaysHolder = weekdaysHolder + dayBody;
+
+
+        });
+
+
+
+
+        var ForecastBody = ' \
+          <div class="forecast"> \
+            '+ weekdaysHolder +'\
+          </div> \
+        ';
+
+        $(place).append(ForecastBody);
+
+
+        
+      }
+
+    }
+
+
+
+    WGen.createSmallWidget('.wather_widget a', xml);
+    WGen.createBigWidget('.big_wather_widget_container', xml);
+
+
+  });
+})(jQuery);
+
