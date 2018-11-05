abba45b097b64a38c99866a747c580b9c917d772
thehatter <andrewlutenko@gmail.com>
2018-11-05 12:08:41 +0300

    more slm components

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,15 +3,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var slm = require("gulp-slm");
-var slim = require("gulp-slim");
-var cached = require('gulp-cached');
+//var slim = require("gulp-slim");
+//var cached = require('gulp-cached');
 
 var injectSvg = require('gulp-inject-svg');
 var injectSvgOptions = { base: '/' };
 
-slim({
-  require: ['slim/include']
-})
 
 gulp.task('sass', function () {
   return gulp.src('./scss/main.scss')
@@ -26,21 +23,32 @@ gulp.task('slm', function() {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('slim', function(){
-  return gulp.src("./*.slim")
-    .pipe(cached('slim'))
-    .pipe(slim({
-      //require: ['slim/include'],
-      pretty: true
-    }))
-    .pipe(injectSvg(injectSvgOptions))
-    .pipe(gulp.dest("./"));
-});
+// gulp.task('slm_components', function() {
+//   return gulp.src('./*.slm')
+//     .pipe(slm())
+//     .pipe(injectSvg(injectSvgOptions))
+//     .pipe(gulp.dest('./'));
+// });
+
+// gulp.task('slim', function(){
+//   return gulp.src("./*.slim")
+//     .pipe(cached('slim'))
+//     .pipe(slim({
+//       //require: ['slim/include'],
+//       pretty: true
+//     }))
+//     .pipe(injectSvg(injectSvgOptions))
+//     .pipe(gulp.dest("./"));
+// });
 
 
 
 gulp.task('default', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
   //gulp.watch('./*.slim', ['slim']);
-  gulp.watch('./*/*.slm', ['slm']);
+  gulp.watch('./*.slm', ['slm']);
+  gulp.watch('./components/*.slm', function(){
+    console.log('component was chenged');
+    gulp.start('slm');
+  });
 });
